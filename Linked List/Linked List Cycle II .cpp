59b98d71a8615db53c7d08da76170265cc2ce9f1
Priyanshu <74262554+priyanshumaurya007@users.noreply.github.com>
2022-01-25 22:52:15 +0530

    Create Linked List Cycle II .cpp

---
+++ Linked List/Linked List Cycle II .cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) 
+    {
+        if(!head || !head->next)
+            return NULL;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while(fast && fast->next && slow)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if(slow==fast)
+            {
+                slow = head;
+                
+                while(slow!=fast)
+                {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                
+                return slow;
+            }
+        }
+        
+        return NULL;
+        
+    }
+};
