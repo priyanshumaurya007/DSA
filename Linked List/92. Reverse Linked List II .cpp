30b04bd31476264df1134620257df164dc02d57f
Priyanshu <74262554+priyanshumaurya007@users.noreply.github.com>
2022-01-28 23:07:43 +0530

    Create 92. Reverse Linked List II .cpp

---
+++ Linked List/92. Reverse Linked List II .cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) 
+    {
+        // if list is empty
+        
+        if(head == NULL)
+            return NULL;
+        
+        // first reach to left pointer
+        
+        ListNode *curr = head , *prev = NULL;
+        while(left>1)
+        {
+            prev = curr;
+            curr = curr->next;
+            right--;
+            left--;
+        }
+        
+        // reverse of list till we reach to right pointer
+        
+        ListNode *con = prev , *tail = curr;
+        
+        
+        while(right>0)
+        {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            right--;
+        }
+        
+        // adjusts of final connections
+        
+        if(con)
+            con->next = prev;
+        else
+            head = prev;
+        
+        tail->next = curr;
+        
+        return head;
+            
+        
+    }
+};
