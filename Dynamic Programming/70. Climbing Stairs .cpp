ba95091cddea5e32323a95b6b55151e66b7f2653
Priyanshu <74262554+priyanshumaurya007@users.noreply.github.com>
2022-02-05 14:38:14 +0530

    Create 70. Climbing Stairs .cpp

---
+++ Dynamic Programming/70. Climbing Stairs .cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    // 1D array for dp
+    int dp[47];
+    
+    // top to bottom approach
+    int climb( int n )
+    {
+        // base case 
+        if( n<0 )
+            return 0;
+        
+        // base case , return will be 1
+        if( n==0 || n==1)
+            return 1;
+        
+        // if we have already calculated then return that value only
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        // otherwise recusion
+        int ans = climb(n-1) + climb(n-2);
+        
+        // put the result into dp array
+        return dp[n] = ans;
+    }
+    
+    int climbStairs(int n) 
+    {
+        // initialize dp with -1
+        for(int i=0;i<47;i++)
+            dp[i]=-1;
+        
+        // return the result
+        return climb(n);
+        
+        
+        
+    }
+};
