9d521d1f4a0f56312e2bb7845302169f0fb34047
Priyanshu <74262554+priyanshumaurya007@users.noreply.github.com>
2022-02-05 14:41:05 +0530

    Create 62. Unique Paths .cpp

---
+++ Dynamic Programming/62. Unique Paths .cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    // 2D array for dp
+    int dp[100][100];
+    
+    // top to bottom approach
+    int rec(int m, int n)
+    {
+        // base case
+        if( m==0 || n==0)
+            return 1;
+        
+        // if we have already calculated the value then return that
+        if(dp[m][n]!=-1)
+            return dp[m][n];
+        
+        // recusion
+        int ans = rec(m-1,n) + rec(m,n-1);
+        
+        // store the value before returing the value
+        return dp[m][n] = ans;
+        
+    }
+    
+    int uniquePaths(int m, int n) 
+    {
+        // initialize the 2D array 
+        for(int i=0 ;i<100 ; i++)
+        {
+            for(int j=0 ; j<100 ;j++)
+                dp[i][j]=-1;
+        }
+        
+        // call the function
+        int ans = rec(m-1 , n-1);
+        
+        // return the result
+        return ans;
+    }
+};
